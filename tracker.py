417b40771a90cae4d037eb3644d6fc2f86b5b1ae
aryansonsurkar <242007347+AryanSonsurkar-Draco-AI@users.noreply.github.com>
2026-01-14 21:03:21 +0530

    updates

--- tracker.py
+++ tracker.py
@@ -16,12 +16,45 @@ def add_habit():
         "streak": 0,
         "done_today": False
     }
+
     with open("habits.json","w") as file:
         json.dump(data,file,indent=4)
     print("habit added successfully!!!")
         
 def mark_done():
-    pass
+    if not os.path.exists("habits.json"):
+        print("No habits found!")
+        return
+    
+    with open("habits.json","r")as file:
+        data = json.load(file)
+
+    habit = input("Enter habit name: ").strip()
+    if habit not in data:
+        print("Habit not found!")
+
+    if data[habit]["done_today"]:
+        print("Habit already marked  done today!")
+        return
+    
+    data[habit]["done_today"] = True
+    data[habit]["streak"] += 1
+
+    with open("habits.json","w")as file:
+        json.dump(data,file,indent=1)
+    print("Habit marked done!!!")
+
+def check_status():
+    if not os.path.exists("habits.json"):
+        print("No habits found!")
+
+    with open("habits.json","r")as file:
+        data = json.load(file)
+
+    print("\nHabit Status: ")
+    for habit, info in data.items():
+        status = "Done" if info["done_today"] else "Not Done!!!"
+        print(f"{habit} | Streak: {info['streak']} | {status}")
 
 def main():
     print("------------------------------")
@@ -41,6 +74,16 @@ def main():
         
         elif choice=="1":
             add_habit()
+
+        elif choice=="2":
+            mark_done()
+
+        elif choice=="3":
+            check_status()
+
+        else:
+            print("Invalid Choice")
+            continue
 
 if __name__=="__main__":
     main()
